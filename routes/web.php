fca69c5fb4996f0b1332fd17c6b0b832bbfc2365
stojan-mitric-devtech <stojan.mitric.contractor@ensighten.com>
2017-05-24 16:46:15 +0200

    Added search by books title

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/books', function() {
 
 Route::post('/books', function(Request $request) {
 
-    $client = new Client(['base_uri' => 'https://www.googleapis.com/books/v1/',
+    $client = new Client(['base_uri' => 'https://www.googleapis.com/books/v1/volumes',
         'verify' => "C:/wamp64/bin/php/php5.6.25/extras/ssl/cacert.pem" ]);
 
     $client->getConfig('config/curl/' . CURLOPT_SSL_VERIFYPEER, false);
@@ -33,20 +33,51 @@ Route::post('/books', function(Request $request) {
 
         if(strlen($request['isbn']) >0) {
 
-            $response = $client->request('GET','volumes?q=isbn:' . $request['isbn']);
+            $response = $client->request('GET','?q=isbn:' . $request['isbn']);
 
-            $result = (string)$response->getBody();
+            //$result = (string)$response->getBody();
+
+            //echo $result;
+
+
+            $result = $response->getBody();
+
+            echo $response->getStatusCode();
+
+            echo '<br>';
+            echo '<br>';
 
             echo $result;
+
+            $data = json_decode($result, true);
+
+            echo '<br>';
+            echo '<br>';
+
+            echo "Book name is: " . $data['items'][0]["volumeInfo"]["title"];
+
+            echo '<br>';
+
+            echo "Book authors are: " . @implode(",",  $data['items'][0]["volumeInfo"]["authors"]);
+
+            echo '<br>';
+
+            echo "Pagecount = " . $data['items'][0]['volumeInfo']['pageCount'];
 
 
         } else if(strlen($request['bookName']) >0){
 
-            /*$response = $client->request('GET', $request["bookName"]);
+            $response = $client->request('GET', '?q=intitle:'. $request["bookName"]);
+
+            $result = $response->getBody();
 
             echo $response->getStatusCode();
-            echo $response->getHeader('content-type');
-            echo $response->getBody();*/
+
+            echo '<br>';
+            echo '<br>';
+
+            echo $result;
+
 
         }else {
             return redirect()->back();
